8c57330728bce25b9923bec762d5c3e045bf3e69
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-10-10 21:28:57 +0300

    928

--- src/pages/products/[pid].jsx
+++ src/pages/products/[pid].jsx
@@ -31,6 +31,10 @@ const ProductPage = () => {
   }
 
   const { id, title, description, price, image } = product;
+  const formattedPrice = new Intl.NumberFormat('en-US', {
+    currency: 'USD',
+    style: 'currency',
+  }).format(price);
 
   return (
     <>
@@ -46,7 +50,39 @@ const ProductPage = () => {
             <CartControl></CartControl>
           </header>
 
-          <section className="container px-4 lg:px-0 mx-auto">sus</section>
+          <section className="mt-16 container px-4 lg:px-0 mx-auto grid gap-8 grid-cols-12">
+            <div className="col-start-1 col-span-5">
+              <img
+                alt={`Image of ${title}`}
+                className="block w-full"
+                src={image}
+              ></img>
+            </div>
+
+            <header className="col-start-7 col-span-6 pt-12">
+              <h1 className="text-2xl uppercase font-medium">{title}</h1>
+              <p className="mt-12">{description}</p>
+
+              <div className="mt-12">
+                <span className="text-3xl leading-9 font-bold">
+                  {formattedPrice}
+                </span>
+              </div>
+
+              <div className="mt-12">
+                <button
+                  className="bg-black text-white uppercase font-medium text-sm py-3 px-6 hover:bg-amber-800 transition-colors"
+                  title={`Add ${title} to cart`}
+                  type="button"
+                  onClick={() => {
+                    alert(id);
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </header>
+          </section>
           <section className="border-t"></section>
           <section className="container px-4 lg:px-0 mx-auto">jos</section>
         </main>
