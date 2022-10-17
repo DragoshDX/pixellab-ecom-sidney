85ba99453f08acaf5a2d3cc196e60dea86a7ba2c
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-10-17 21:43:26 +0300

    943

--- src/components/cart/CartControl.jsx
+++ src/components/cart/CartControl.jsx
@@ -1,9 +1,23 @@
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from '../../pages/_app';
 
 export const CartControl = () => {
   const { cart } = useContext(AppContext);
+
+  if (cart === null) {
+    return <></>;
+  }
+
+  const { products } = cart;
+
+  const cartQty = products.reduce((cartQty, product) => {
+    const { quantity } = product;
+
+    cartQty += quantity;
+
+    return cartQty;
+  }, 0);
 
   return (
     <ul className="border border-zinc-400">
@@ -13,7 +27,7 @@ export const CartControl = () => {
             className="w-24 h-24 flex justify-center items-center"
             title="Cart"
           >
-            {cart.qty}
+            {cartQty}
           </a>
         </Link>
       </li>
