7fffbf693f79dfe5b95361079159055152adae79
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-10-24 22:03:24 +0300

    1003

--- src/components/catalog/GridControls.jsx
+++ src/components/catalog/GridControls.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useIsMobile } from '../../hooks';
+import { useIsMobile, useLocalStorage } from '../../hooks';
 
 const buttonClasses = `w-24 h-24 border-l border-zinc-400 flex justify-center items-center`;
 
 export const GridControls = ({ setPerRow = () => {} }) => {
-  const [itemsPerRow, setItemsPerRow] = useState('4/row');
+  const [itemsPerRow, setItemsPerRow] = useLocalStorage('perRow', '4/row');
   const isMobile = useIsMobile();
 
   // de fiecare data cand itemsPerRow se schimba
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export * from './useProducts';
 export * from './useCart';
 export * from './useProduct';
 export * from './useIsMobile';
+export * from './useLocalStorage';
