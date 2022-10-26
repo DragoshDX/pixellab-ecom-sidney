e1b30654b1fed9d587a8746668a9c936618523ba
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-10-26 21:47:58 +0300

    947

--- src/components/catalog/AddToCart.jsx
+++ src/components/catalog/AddToCart.jsx
@@ -2,27 +2,8 @@ import { useContext } from 'react';
 import { baseUrl } from '../..';
 import { AppContext } from '../../pages/_app';
 
-const alterCart = (cart, productId) => {
-  const { products } = cart;
-
-  const product = products.find((product) => {
-    return product.id === productId;
-  });
-
-  if (product === undefined) {
-    products.push({
-      productId,
-      quantity: 1,
-    });
-  } else {
-    product.quantity += 1;
-  }
-
-  return cart;
-};
-
 export const AddToCart = ({ product }) => {
-  const { cart, setCart } = useContext(AppContext);
+  const { cart, addProduct } = useContext(AppContext);
 
   if (cart === null) {
     return;
@@ -45,10 +26,7 @@ export const AddToCart = ({ product }) => {
         return response.json();
       })
       .then((_) => {
-        const oldCart = alterCart(cart, productId);
-        const newCart = { ...oldCart };
-
-        setCart(newCart);
+        addProduct(productId);
       });
   };
 
